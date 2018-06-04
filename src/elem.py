ac34f54c7f56b0d63e72f077b479e90cd4b066cc
wijtse <wijtse@rekkerit.nl>
2018-06-04 14:15:18 +0200

    code cleanup

--- src/elem.py
+++ src/elem.py
@@ -7,45 +7,32 @@ class Elem:
         self.list = l
 
     def __eq__(self, other):
-        self.list.comparisons += 1
-        self.list.high_lighted1 = self.x
-        self.list.high_lighted2 = other.x
-        self.list.view.update(self.list, True)
+        self.notify_list(other)
         return self.x == other.x
 
     def __ne__(self, other):
-        self.list.comparisons += 1
-        self.list.high_lighted1 = self.x
-        self.list.high_lighted2 = other.x
-        self.list.view.update(self.list, True)
+        self.notify_list(other)
         return self.x != other.x
 
     def __lt__(self, other):
-        self.list.comparisons += 1
-        self.list.high_lighted1 = self.x
-        self.list.high_lighted2 = other.x
-        self.list.view.update(self.list, True)
+        self.notify_list(other)
         return self.x < other.x
 
     def __le__(self, other):
-        self.list.comparisons += 1
-        self.list.high_lighted1 = self.x
-        self.list.high_lighted2 = other.x
-        self.list.view.update(self.list, True)
+        self.notify_list(other)
         return self.x <= other.x
 
     def __gt__(self, other):
-        self.list.comparisons += 1
-        self.list.high_lighted1 = self.x
-        self.list.high_lighted2 = other.x
-        self.list.view.update(self.list, True)
+        self.notify_list(other)
         return self.x > other.x
 
     def __ge__(self, other):
+        self.notify_list(other)
+        return self.x >= other.x
+
+    def notify_list(self, other):
         self.list.comparisons += 1
         self.list.high_lighted1 = self.x
         self.list.high_lighted2 = other.x
         self.list.view.update(self.list, True)
-        return self.x >= other.x
-
 
